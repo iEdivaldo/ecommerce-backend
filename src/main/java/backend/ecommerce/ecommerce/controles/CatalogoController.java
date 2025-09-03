0a71fff2d987ad819112c4e8cb8af642fdb53c6b
Edivaldo José dos Santos <dalsantos10@gmail.com>
2025-09-03 16:28:28 -0300

    Arrumando problema de conexão com front end

--- src/main/java/backend/ecommerce/ecommerce/controles/CatalogoController.java
+++ src/main/java/backend/ecommerce/ecommerce/controles/CatalogoController.java
@@ -2,6 +2,7 @@ package backend.ecommerce.ecommerce.controles;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import backend.ecommerce.ecommerce.entidades.Produto;
 import backend.ecommerce.ecommerce.repositorios.ProdutoRepositorio;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/produtos")
 public class CatalogoController {
